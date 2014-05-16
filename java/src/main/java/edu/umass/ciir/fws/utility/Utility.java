3151c775d06453cd032941f4adf1d700e97703e4
Weize Kong <wkong@cs.umass.edu>
2014-05-15 20:54:58 -0400

    pool facets

--- java/src/main/java/edu/umass/ciir/fws/utility/Utility.java
+++ java/src/main/java/edu/umass/ciir/fws/utility/Utility.java
@@ -208,7 +208,7 @@ public class Utility extends org.lemurproject.galago.tupleflow.Utility {
         String facetDir = run.get(0).toString();
         String model = run.get(1).toString();
         String name;
-        if (run.size() == 3) {
+        if (run.size() >= 3) {
             name = String.format("%s.%s.%s.facet", qid, model, parametersToFileNameString(run.subList(2, run.size())));
         } else {
             name = String.format("%s.%s.facet", qid, model);
