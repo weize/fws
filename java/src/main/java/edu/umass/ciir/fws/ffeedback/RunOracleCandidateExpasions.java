b8cd829adaf385841e8bfeb181c9d56a6676371d
Weize Kong <wkong@cs.umass.edu>
2014-06-03 09:49:09 -0400

    oracle

--- java/src/main/java/edu/umass/ciir/fws/ffeedback/RunOracleCandidateExpasions.java
+++ java/src/main/java/edu/umass/ciir/fws/ffeedback/RunOracleCandidateExpasions.java
@@ -9,7 +9,9 @@ import edu.umass.ciir.fws.anntation.FeedbackTerm;
 import edu.umass.ciir.fws.clustering.FacetModelParamGenerator;
 import edu.umass.ciir.fws.clustering.ScoredFacet;
 import edu.umass.ciir.fws.clustering.ScoredItem;
+import edu.umass.ciir.fws.query.QueryFileParser;
 import edu.umass.ciir.fws.query.QueryTopicSubtopicMap;
+import edu.umass.ciir.fws.types.TfQuery;
 import edu.umass.ciir.fws.types.TfQueryExpansion;
 import edu.umass.ciir.fws.utility.Utility;
 import java.io.BufferedWriter;
@@ -54,9 +56,13 @@ public class RunOracleCandidateExpasions extends AppFunction {
         Job job = new Job();
 
         job.add(getSplitStage(parameters));
+        job.add(getSplitExpansionStage(parameters));
+        job.add(getCmbExpansionStage(parameters));
         job.add(getProcessStage(parameters));
 
-        job.connect("split", "process", ConnectionAssignmentType.Each);
+        job.connect("split", "splitExpansion", ConnectionAssignmentType.Each);
+        job.connect("splitExpansion", "cmbExpansion", ConnectionAssignmentType.Combined);
+        job.connect("cmbExpansion", "process", ConnectionAssignmentType.Each);
 
         return job;
     }
@@ -71,37 +77,61 @@ public class RunOracleCandidateExpasions extends AppFunction {
     private Stage getSplitStage(Parameters parameter) {
         Stage stage = new Stage("split");
 
-        stage.addOutput("expansions", new TfQueryExpansion.QidModelExpIdOrder());
+        stage.addOutput("queries", new TfQuery.IdOrder());
 
         Parameters p = new Parameters();
         p.set("input", new ArrayList());
         p.getList("input").add(new File(parameter.getString("queryFile")).getAbsolutePath());
 
         stage.add(new Step(FileSource.class, p));
-        stage.add(new Step(ExpandQueryWithSingleFacetTerm.class, parameter));
+        stage.add(new Step(QueryFileParser.class));
+        stage.add(Utility.getSorter(new TfQuery.IdOrder()));
+        stage.add(new OutputStep("queries"));
+
+        return stage;
+    }
+
+    private Stage getSplitExpansionStage(Parameters parameters) {
+        Stage stage = new Stage("splitExpansions");
+
+        stage.addInput("queries", new TfQuery.IdOrder());
+        stage.addOutput("expansions", new TfQueryExpansion.QidModelExpIdOrder());
+
+        stage.addInput("queries", new TfQuery.IdOrder());
+        stage.add(new Step(ExpandQueryWithSingleFacetTerm.class, parameters));
         stage.add(Utility.getSorter(new TfQueryExpansion.QidModelExpIdOrder()));
-        stage.add(new Step(UniqueQueryExpansion.class));
-        stage.add(new Step(WriteExpansionFile.class, parameter));
         stage.add(new OutputStep("expansions"));
+        return stage;
+    }
+
+    private Stage getCmbExpansionStage(Parameters parameters) {
+        Stage stage = new Stage("cmbExpansions");
+
+        stage.addInput("expansions", new TfQueryExpansion.QidModelExpIdOrder());
+        stage.addOutput("expansions2", new TfQueryExpansion.QidModelExpIdOrder());
+
+        stage.addInput("expansions", new TfQuery.IdOrder());
+        stage.add(new Step(UniqueQueryExpansion.class));
+        stage.add(new Step(WriteExpansionFile.class, parameters));
+        stage.add(new OutputStep("expansions2"));
         return stage;
     }
 
     private Stage getProcessStage(Parameters parameters) {
         Stage stage = new Stage("process");
 
-        stage.addInput("expansions", new TfQueryExpansion.QidModelExpIdOrder());
+        stage.addInput("expansions2", new TfQueryExpansion.QidModelExpIdOrder());
 
-        stage.add(new InputStep("expansions"));
+        stage.add(new InputStep("expansions2"));
         stage.add(new Step(RunExpandedQuery.class, parameters));
         return stage;
     }
 
     @Verified
-    @InputClass(className = "org.lemurproject.galago.tupleflow.types.FileName")
+    @InputClass(className = "edu.umass.ciir.fws.types.TfQuery")
     @OutputClass(className = "edu.umass.ciir.fws.types.TfQueryExpansion")
-    public static class ExpandQueryWithSingleFacetTerm extends StandardStep<FileName, TfQueryExpansion> {
+    public static class ExpandQueryWithSingleFacetTerm extends StandardStep<TfQuery, TfQueryExpansion> {
 
-        QueryTopicSubtopicMap queryMap;
         FacetModelParamGenerator modelParams;
 
         String allFacetDir;
@@ -109,23 +139,12 @@ public class RunOracleCandidateExpasions extends AppFunction {
         String expansionModel;
         Parameters p;
         ExpansionDirectory expansionDir;
+        List<String> facetSources;
 
         public ExpandQueryWithSingleFacetTerm(TupleFlowParameters parameters) throws IOException {
             p = parameters.getJSON();
-        }
-
-        @Override
-        public void process(FileName file) throws IOException {
-
-        }
-
-        @Override
-        public void close() throws IOException {
             // setup
             expansionModel = p.getString("expansionModel");
-            File selectionFile = new File(p.getString("subtopicSelectedIdFile"));
-            File queryFile = new File(p.getString("queryFile"));
-            queryMap = new QueryTopicSubtopicMap(selectionFile, queryFile);
             modelParams = new FacetModelParamGenerator(p);
             allFacetDir = p.getString("facetDir");
             expansionDir = new ExpansionDirectory(p);
@@ -134,19 +153,22 @@ public class RunOracleCandidateExpasions extends AppFunction {
             } else {
                 expIdMap = new ExpansionIdMap();
             }
+            facetSources = p.getAsList("facetSources");
+        }
 
-            // emits
-            List<String> facetSources = p.getAsList("facetSources");
+        @Override
+        public void process(TfQuery query) throws IOException {
             for (String source : facetSources) {
                 List<String> params = modelParams.getParams(source);
                 for (String param : params) {
-                    for (String qid : queryMap.getQidSet()) {
-                        processAndEmit(source, qid, param);
-                    }
+                    processAndEmit(source, query, param);
 
                 }
             }
+        }
 
+        @Override
+        public void close() throws IOException {
             // closing
             processor.close();
             Utility.infoOpen(expansionDir.expansionIdFile);
@@ -155,21 +177,21 @@ public class RunOracleCandidateExpasions extends AppFunction {
 
         }
 
-        private void processAndEmit(String source, String qid, String param) throws IOException {
+        private void processAndEmit(String source, TfQuery query, String param) throws IOException {
             String facetDir = Utility.getFileName(allFacetDir, source, "facet");
-            File facetFile = new File(Utility.getFacetFileName(facetDir, qid, source, param));
+            File facetFile = new File(Utility.getFacetFileName(facetDir, query.id, source, param));
             List<ScoredFacet> facets = ScoredFacet.loadFacets(facetFile);
 
             // only use top 30
             for (ScoredFacet facet : facets.subList(0, Math.min(facets.size(), 30))) {
-                String oriQuery = queryMap.getQuery(qid);
+                String oriQuery = query.text;
 
                 int fidx = 0;
                 int tidx = 0;
 
                 for (ScoredItem item : facet.items) {
                     FeedbackTerm ft = new FeedbackTerm(item.item, fidx, tidx);
-                    QueryExpansion qe = new QueryExpansion(qid, oriQuery, expansionModel, ft.toString(), expIdMap);
+                    QueryExpansion qe = new QueryExpansion(query.id, oriQuery, expansionModel, ft.toString(), expIdMap);
                     qe.expand();
                     processor.process(qe.toTfQueryExpansion());
                 }
