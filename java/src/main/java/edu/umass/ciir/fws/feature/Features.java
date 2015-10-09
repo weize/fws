7519ec0cab1a4ad9ad14aa602444e833d3c9cd83
Weize Kong <wkong@cs.umass.edu>
2015-10-09 16:24:16 -0400

    Add generic Feature class

---
+++ java/src/main/java/edu/umass/ciir/fws/feature/Features.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.umass.ciir.fws.feature;
+
+import edu.umass.ciir.fws.utility.TextProcessing;
+
+/**
+ *
+ * @author wkong
+ */
+public class Features {
+
+    Object[] features;
+
+    public void setFeature(Object value, int idx) {
+        this.features[idx] = value;
+    }
+
+    public Object getFeature(int idx) {
+        return this.features[idx];
+    }
+
+    public String featuresToString() {
+        return TextProcessing.join(features, "\t");
+    }
+}
