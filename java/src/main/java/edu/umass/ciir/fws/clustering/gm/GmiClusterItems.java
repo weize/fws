b380132e8a36d89f5d55b1fc120350fa11fb2306
Weize Kong <wkong@cs.umass.edu>
2015-12-18 17:17:41 -0500

    gmi cluster items for tuning

---
+++ java/src/main/java/edu/umass/ciir/fws/clustering/gm/GmiClusterItems.java
@@ -0,0 +1,154 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.umass.ciir.fws.clustering.gm;
+
+import edu.umass.ciir.fws.clustering.gm.GmLearn.DoNonethingForQueryParams;
+import edu.umass.ciir.fws.query.QueryFileParser;
+import edu.umass.ciir.fws.types.TfQuery;
+import edu.umass.ciir.fws.types.TfQueryParameters;
+import edu.umass.ciir.fws.utility.Utility;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.lemurproject.galago.core.tools.AppFunction;
+import org.lemurproject.galago.tupleflow.FileSource;
+import org.lemurproject.galago.tupleflow.InputClass;
+import org.lemurproject.galago.tupleflow.OutputClass;
+import org.lemurproject.galago.tupleflow.Parameters;
+import org.lemurproject.galago.tupleflow.StandardStep;
+import org.lemurproject.galago.tupleflow.TupleFlowParameters;
+import org.lemurproject.galago.tupleflow.execution.ConnectionAssignmentType;
+import org.lemurproject.galago.tupleflow.execution.InputStep;
+import org.lemurproject.galago.tupleflow.execution.Job;
+import org.lemurproject.galago.tupleflow.execution.OutputStep;
+import org.lemurproject.galago.tupleflow.execution.Stage;
+import org.lemurproject.galago.tupleflow.execution.Step;
+import org.lemurproject.galago.tupleflow.execution.Verified;
+import org.lemurproject.galago.tupleflow.types.FileName;
+
+/**
+ * cluster gmi for cross-validation.
+ * cluster items for train queries in each folders for different parameter combination.
+ * Will be used for tuning.
+ * @author wkong
+ */
+public class GmiClusterItems extends AppFunction {
+
+    @Override
+    public String getName() {
+        return "cluster-gmi-tune";
+    }
+
+    @Override
+    public String getHelpString() {
+        return "fws " + getName() + " [parameters...]\n"
+                + AppFunction.getTupleFlowParameterString();
+    }
+
+    @Override
+    public void run(Parameters p, PrintStream output) throws Exception {
+        Job job = createJob(p);
+        AppFunction.runTupleFlowJob(job, p, output);
+
+    }
+
+    private Job createJob(Parameters parameters) {
+        Job job = new Job();
+
+        job.add(getSplitStage(parameters));
+        job.add(getClusterStage(parameters));
+
+        job.connect("split", "cluster", ConnectionAssignmentType.Each);
+
+        return job;
+    }
+
+    private Stage getSplitStage(Parameters parameter) {
+        Stage stage = new Stage("split");
+
+        stage.addOutput("clusters", new TfQueryParameters.IdParametersOrder());
+
+        List<String> inputFiles = parameter.getAsList("queryFile");
+
+        Parameters p = new Parameters();
+        p.set("input", new ArrayList());
+        for (String input : inputFiles) {
+            p.getList("input").add(new File(input).getAbsolutePath());
+        }
+
+        stage.add(new Step(FileSource.class, p));
+        stage.add(new Step(SplitTuneRuns.class, parameter));
+        stage.add(Utility.getSorter(new TfQueryParameters.IdParametersOrder()));
+        stage.add(new OutputStep("clusters"));
+
+        return stage;
+    }
+
+    private Stage getClusterStage(Parameters parameters) {
+        Stage stage = new Stage("cluster");
+
+        stage.addInput("clusters", new TfQueryParameters.IdParametersOrder());
+
+        stage.add(new InputStep("clusters"));
+        stage.add(new Step(GmiClusterer.class, parameters));
+        stage.add(new Step(AppendFacetRankerParameter.class));
+        stage.add(new Step(GmiClusterToFacetConverter.class, parameters));
+        stage.add(new Step(DoNonethingForQueryParams.class));
+
+        return stage;
+    }
+
+    @Verified
+    @InputClass(className = "org.lemurproject.galago.tupleflow.types.FileName")
+    @OutputClass(className = "edu.umass.ciir.fws.types.TfQueryParameters")
+    public static class SplitTuneRuns extends StandardStep<FileName, TfQueryParameters> {
+
+        long numFolders;
+        List<Double> termProbThs;
+        List<Double> pairProbThs;
+        String trainDir;
+        String[] rankers = new String[]{"sum", "avg"};
+        List<Long> metricIndices;
+        BufferedWriter writer;
+
+        public SplitTuneRuns(TupleFlowParameters parameters) throws IOException {
+            Parameters p = parameters.getJSON();
+            numFolders = parameters.getJSON().getLong("cvFolderNum");
+            termProbThs = p.getAsList("gmiTermProbThesholds", Double.class);
+            pairProbThs = p.getAsList("gmiPairProbThesholds", Double.class);
+
+            String gmDir = p.getString("gmDir");
+            trainDir = Utility.getFileName(gmDir, "train");
+            metricIndices = p.getAsList("facetTuneMetricIndices", Long.class);
+            writer = Utility.getWriter(p.getString("gmiTunedParamFile"));
+        }
+
+        @Override
+        public void process(FileName object) throws IOException {
+        }
+
+        @Override
+        public void close() throws IOException {
+            for (int i = 1; i <= numFolders; i++) {
+                String folderId = String.valueOf(i);
+                String trainQuery = Utility.getFileName(trainDir, String.valueOf(i), "train.query");
+                for (TfQuery query : QueryFileParser.loadQueryList(trainQuery)) {
+                    for (double termTh : termProbThs) {
+                        for (double pairTh : pairProbThs) {
+                            String newParams = Utility.parametersToString(folderId, "tune", termTh, pairTh);
+                            processor.process(new TfQueryParameters(query.id, query.text, newParams));
+                        }
+                    }
+                }
+            }
+            processor.close();
+        }
+    }
+
+}
