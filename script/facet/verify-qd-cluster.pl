8c9fabf0a8fb1a9606ddf47002c29a6536a06c3d
Weize Kong <wkong@cs.umass.edu>
2015-12-22 13:58:27 -0500

    update verify qd cluster

--- script/facet/verify-qd-cluster.pl
+++ script/facet/verify-qd-cluster.pl
@@ -1,9 +1,11 @@
 use strict;
 my $path = $ARGV[0];
 print "$path\n";
-my @files = <$path>;
+my @files = </home2/wkong/work/tmp/stderr/galago-process-*>;
 
 my %opens;
+my $openCount = 0;
+my $closeCount = 0;
 for my $file (@files) {
 	open my $in, $file or die "Cannot open $file";
 	while(my $line = <$in>) {
@@ -11,10 +13,12 @@ for my $file (@files) {
 		if ($line =~ /^File opened (.+)$/) {
 			my $open = $1; 
 			$opens{$open} = 1;
+			$openCount ++;
 		}
 		if ($line =~ /^Writte in (.+)$/) {
 			my $open = $1; 
 			$opens{$open} = 0; 
+			$closeCount ++;
 		}
 	}
 	close $in;
@@ -26,3 +30,4 @@ for my $key (keys %opens) {
 		print "$key\n";
 	}
 }
+print "$openCount\t$closeCount\n";
